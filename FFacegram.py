39e22d18bd9e23b9594b9985b19779f97d53a825
naza4k0 <36163620+naza4k0@users.noreply.github.com>
2018-11-06 17:46:29 +0200

    update 0.1e

--- FFacegram.py
+++ FFacegram.py
@@ -1,3 +1,31 @@
-import telegram
-bot = telegram.bot(token='757304961:AAFFLGinPle1bE5Arq12SL2mFiGj_DL849E')
-print(bot.get_me())
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Привет, используй комманду /girls для того чтобы принять участие в голосовании")
+
+
+def girls(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Го сэкс")
+
+
+def unknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
+
+
+updater = Updater(token='757304961:AAFFLGinPle1bE5Arq12SL2mFiGj_DL849E')
+
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+girls_handler = CommandHandler('girls', girls)
+dispatcher.add_handler(girls_handler)
+
+updater.start_polling()
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
