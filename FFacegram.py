a26ba7dff3aab9937d3b9b85ba2f36be2c45fec2
naza4k0 <36163620+naza4k0@users.noreply.github.com>
2018-11-13 14:55:10 +0200

    arsen 4

--- FFacegram.py
+++ FFacegram.py
@@ -31,11 +31,22 @@ def photo_upd(bot, update):
 def g_vote(bot, update):
     g_vote2(bot, update.message.chat_id)
 
+def m_vote(bot, update):
+    m_vote2(bot, update.message.chat_id)
+
 def g_nrating(bot, update):
     rates = Pyre().g_rating()
     girls_number = 5
     emojis = ['🥇', '🥈', '🥉', '👩‍', '👩‍']
     girls_titles = [Pyre().g_link(rates[i]) for i in range(girls_number)]
+    textr = '\n'.join([f'{e} : {name_with_link}' for e, name_with_link in zip(emojis, girls_titles)])
+    bot.send_message(chat_id=update.message.chat_id, text=textr, parse_mode="HTML", disable_web_page_preview=True)
+
+def m_nrating(bot, update):
+    rates = Pyre().m_rating()
+    men_number = 5
+    emojis = ['🥇', '🥈', '🥉', '👦🏻', '👦🏻']
+    girls_titles = [Pyre().m_link(rates[i]) for i in range(men_number)]
     textr = '\n'.join([f'{e} : {name_with_link}' for e, name_with_link in zip(emojis, girls_titles)])
     bot.send_message(chat_id=update.message.chat_id, text=textr, parse_mode="HTML", disable_web_page_preview=True)
 
@@ -48,6 +59,15 @@ def g_idrating(bot, update):
     textr = '\n'.join([f'{e} : {name_with_link}' for e, name_with_link in zip(emojis, girls_titles)])
     bot.send_message(chat_id=update.message.chat_id, text=textr, parse_mode="HTML", disable_web_page_preview=True)
 
+def m_idrating(bot, update):
+    Pyre().mreg_check(update.message.chat_id)
+    rates = Pyre().mid_rating(update.message.chat_id)
+    men_number = 5
+    emojis = ['🥇', '🥈', '🥉', '👦🏻', '👦🏻']
+    girls_titles = [Pyre().m_link(rates[i]) for i in range(men_number)]
+    textr = '\n'.join([f'{e} : {name_with_link}' for e, name_with_link in zip(emojis, girls_titles)])
+    bot.send_message(chat_id=update.message.chat_id, text=textr, parse_mode="HTML", disable_web_page_preview=True)
+
 def g_adrating(bot, update):
     if from_admins(update):
         rates = Pyre().g_admin_rating()
@@ -56,10 +76,11 @@ def g_adrating(bot, update):
     else:
         bot.send_message(chat_id=update.message.chat_id, text="Sorry, you need admin rights to do that.")
 
-def update(bot, update):
-    if from_super_admin(update):
-        Pyre().g_updater()
-        bot.send_message(chat_id=update.message.chat_id, text="Update successful.")
+def m_adrating(bot, update):
+    if from_admins(update):
+        rates = Pyre().m_admin_rating()
+        rating_list_text = '\n'.join([f'{i+1}. {Pyre().m_name(rate)}' for i, rate in enumerate(rates)])
+        bot.send_message(chat_id=update.message.chat_id, text=rating_list_text)
     else:
         bot.send_message(chat_id=update.message.chat_id, text="Sorry, you need admin rights to do that.")
 
@@ -77,22 +98,44 @@ def g_vote2(bot, chat_id):
     x2 = x[1].message_id
     print(x)
     bot.send_message(chat_id=chat_id, text="Какая девушка лучше? 🍓　　　", reply_markup = InlineKeyboardMarkup(
-        [[InlineKeyboardButton(text="Левая", callback_data= f'{a} {b} {x1} {x2}'), InlineKeyboardButton(text="Правая", callback_data=f'{b} {a} {x1} {x2}')]]))
+        [[InlineKeyboardButton(text="Левая", callback_data= f'{a} {b} {x1} {x2} {1}'), InlineKeyboardButton(text="Правая", callback_data=f'{b} {a} {x1} {x2} {1}')]]))
+   
+def m_vote2(bot, chat_id):
+    a=0
+    b=0
+
+    while a==b or abs(Pyre().m_score(a)-Pyre().m_score(b))>=100:
+       a=random.randint(0,Pyre().m_number()-1)
+       b=random.randint(0,Pyre().m_number()-1)
+
+    media = [telegram.InputMediaPhoto(Pyre().m_photo(a)), telegram.InputMediaPhoto(Pyre().m_photo(b))]
+    x = bot.send_media_group(chat_id=chat_id, media=media)
+    x1 = x[0].message_id
+    x2 = x[1].message_id
+    print(x)
+    bot.send_message(chat_id=chat_id, text="Какой парень лучше? 🍆　　　", reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton(text="Левый", callback_data= f'{a} {b} {x1} {x2} {0}'), InlineKeyboardButton(text="Правый", callback_data=f'{b} {a} {x1} {x2} {0}')]]))
    
 def unknown(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
 
 def handle_query(bot, update):
     data = update.callback_query.data
-    a, b, x1, x2= data.split()
+    a, b, x1, x2, d = data.split()
     c = update.callback_query.message.chat.id
     bot.delete_message(c,x1)
     bot.delete_message(c,x2)
     update.callback_query.message.delete()
-    Pyre().reg_check(c)
-    Pyre().g_round(a, b)
-    Pyre().g1_round(a, b, c)
-    g_vote2(bot, c)
+    if d==1:
+        Pyre().reg_check(c)
+        Pyre().g_round(a, b)
+        Pyre().g1_round(a, b, c)
+        g_vote2(bot, c)
+    else:
+        Pyre().mreg_check(c)
+        Pyre().m_round(a, b)
+        Pyre().m1_round(a, b, c)
+        m_vote2(bot, c)
     
 updater = Updater(token='757304961:AAFFLGinPle1bE5Arq12SL2mFiGj_DL849E')
 dispatcher = updater.dispatcher
@@ -111,14 +154,22 @@ dispatcher.add_handler(start_handler)
 girls_handler = CommandHandler('vote', g_vote)
 rating_handler = CommandHandler('rating', g_nrating)
 idrating_handler = CommandHandler('irating', g_idrating)
-updater_handler = CommandHandler('update', update)
 adrating_handler = CommandHandler('admin_rating', g_adrating)
 
+mgirls_handler = CommandHandler('mvote', m_vote)
+mrating_handler = CommandHandler('mrating', m_nrating)
+midrating_handler = CommandHandler('imrating', m_idrating)
+madrating_handler = CommandHandler('madmin_rating', m_adrating)
+
 dispatcher.add_handler(adrating_handler)
-dispatcher.add_handler(updater_handler)
 dispatcher.add_handler(girls_handler)
 dispatcher.add_handler(rating_handler)
 dispatcher.add_handler(idrating_handler)
+
+dispatcher.add_handler(madrating_handler)
+dispatcher.add_handler(mgirls_handler)
+dispatcher.add_handler(mrating_handler)
+dispatcher.add_handler(midrating_handler)
 
 updater.dispatcher.add_handler(CallbackQueryHandler(handle_query))
 updater.start_polling()
--- pyre.py
+++ pyre.py
@@ -47,6 +47,7 @@ class Pyre():
         self.pyre.child("id_g").child(c).update({a : av1})
         self.pyre.child("id_g").child(c).update({b : bv1})
 
+    
     def g_photo(self, a):
         return self.pyre.child("users").child("photos").child(a).get().val()
 
@@ -78,8 +79,8 @@ class Pyre():
         return maxim
 
     def g_text_upload(self, a):
-        c = self.pyre.child("users").child("number").child(0).get().val()
-        self.pyre.child("users").child("names").child(c).set(a)
+        c = self.pyre.child("musers").child("number").child(0).get().val()
+        self.pyre.child("musers").child("names").child(c).set(a)
        
     def reg_check(self, a):
         xstr = lambda s: s or ""
@@ -90,20 +91,16 @@ class Pyre():
             self.pyre.child("id_g").child("reg").child(a).set(1)
 
     def g_photo_upload(self, a):
-        c = self.pyre.child("users").child("number").child(0).get().val()
-        self.pyre.child("users").child("scores").child(c).set(1000)
-        self.pyre.child("users").child("photos").child(c).set(a)
-        self.pyre.child("users").child("number").update({0: c+1})
+        c = self.pyre.child("musers").child("number").child(0).get().val()
+        self.pyre.child("musers").child("scores").child(c).set(1000)
+        self.pyre.child("musers").child("photos").child(c).set(a)
+        self.pyre.child("musers").child("number").update({0: c+1})
    
     def g_number(self):
         return self.pyre.child("users").child("number").child(0).get().val()
 
     def g_score(self, a):
         return self.pyre.child("users").child("scores").child(a).get().val()
-
-    def g_updater(self):
-        for i in range(self.pyre.child("users").child("number").child(0).get().val()):
-            self.pyre.child("users").child("scores").update({i: 1000})
 
     def g_link(self, a):
         xstr = lambda s: s or ""
@@ -121,6 +118,102 @@ class Pyre():
             data.append(self.pyre.child("users").child("scores").child(i).get().val())
         maxim = []
         for i in range(self.pyre.child("users").child("number").child(0).get().val()):
+            a = max(data)
+            b = data.index(a)
+            data[b] = 0
+            maxim.append(b)
+        return maxim
+
+
+
+
+        def g_round(self, a, b):
+        av = self.pyre.child("users").child("scores").child(a).get().val()
+        bv = self.pyre.child("users").child("scores").child(b).get().val()
+
+        e_a = 1/(1+10**((bv-av)/400))
+        e_b = 1/(1+10**((av-bv)/400))
+
+        av1 = av + 10*(1-e_a) 
+        bv1 = bv - 10*e_b
+        
+        self.pyre.child("users").child("scores").update({a : av1})
+        self.pyre.child("users").child("scores").update({b : bv1})
+
+    def g1_round(self, a, b, c):
+        av = self.pyre.child("id_g").child(c).child(a).get().val()
+        bv = self.pyre.child("id_g").child(c).child(b).get().val()
+
+        e_a = 1/(1+10**((bv-av)/400))
+        e_b = 1/(1+10**((av-bv)/400))
+
+        av1 = av + 10*(1-e_a) 
+        bv1 = bv - 10*e_b
+        
+        self.pyre.child("id_g").child(c).update({a : av1})
+        self.pyre.child("id_g").child(c).update({b : bv1})
+
+    
+    def m_photo(self, a):
+        return self.pyre.child("musers").child("photos").child(a).get().val()
+
+    def m_name(self, a):
+        return self.pyre.child("musers").child("names").child(a).get().val()
+    
+    def m_rating(self):
+        data = []
+        for i in range(self.pyre.child("musers").child("number").child(0).get().val()):
+            data.append(self.pyre.child("musers").child("scores").child(i).get().val())
+        maxim = []
+        for i in range(5):
+            a = max(data)
+            b = data.index(a)
+            data[b] = 0
+            maxim.append(b)
+        return maxim
+
+    def mid_rating(self, a):
+        data = []
+        for i in range(self.pyre.child("musers").child("number").child(0).get().val()):
+            data.append(self.pyre.child("mid_g").child(a).child(i).get().val())
+        maxim = []
+        for i in range(5):
+            a = max(data)
+            b = data.index(a)
+            data[b] = 0
+            maxim.append(b)
+        return maxim
+
+    def mreg_check(self, a):
+        xstr = lambda s: s or ""
+        b = xstr(self.pyre.child("mid_g").child("reg").child(a).get().val())
+        if b == "":
+            for i in range(self.pyre.child("musers").child("number").child(0).get().val()):
+                self.pyre.child("mid_g").child(a).child(i).set(1000)
+            self.pyre.child("mid_g").child("reg").child(a).set(1)
+
+    def m_number(self):
+        return self.pyre.child("musers").child("number").child(0).get().val()
+
+    def m_score(self, a):
+        return self.pyre.child("musers").child("scores").child(a).get().val()
+
+    def m_link(self, a):
+        xstr = lambda s: s or ""
+        link = xstr(self.pyre.child("musers").child("links").child(a).get().val())
+        c = self.pyre.child("musers").child("names").child(a).get().val()
+        if link:
+            b = '<a href="' + link +'">' + c + '</a>'
+        else:
+            b = c
+        return b
+
+    def m_admin_rating(self):
+        data = []
+        for i in range(self.pyre.child("musers").child("number").child(0).get().val()):
+            data.append(self.pyre.child("musers").child("scores").child(i).get().val())
+        maxim = []
+        for i in range(self.pyre.child("musers").child("number").child(0).get().val()):
             a = max(data)
             b = data.index(a)
             data[b] = 0
