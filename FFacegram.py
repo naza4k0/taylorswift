0becc6c95be147cf3391de4fca135971e0654eed
Nazar <36163620+naza4k0@users.noreply.github.com>
2019-10-25 23:57:11 +0300

    Update FFacegram.py

--- FFacegram.py
+++ FFacegram.py
@@ -5,27 +5,41 @@ from pyre import Pyre
 import logging
 import random
 
+def from_admin(update):
+        admins_ids = [367989051]
+        return (update.message.from_user.id in admins_ids)
+
 def start(bot, update):
-        bot.send_message(chat_id=update.message.chat_id, text="Send your media mark in a format \n\n\"name\" type score \n\nFor example, \n\n\"Iron Man\" movie 10 \n\nDon\'t forget to add a season number after name if you are sending a series score.")
+        bot.send_message(chat_id=update.message.chat_id, text="Send your audio your want to add to the library or use the command /music to get a random track from it.")
 
 def text_upd(bot, update):
         bot.forward_message(chat_id = 367989051, from_chat_id=update.message.chat_id, message_id=update.message.message_id)
-        bot.send_message(chat_id=update.message.chat_id, text="Thank you for your mark. It will be added to the system soon.")
+        
+def music_upd(bot, update):
+        if from_admin(update):
+                Pyre().music_upload(update.message.audio.file_id)
+        else:
+                bot.forward_message(chat_id = 367989051, from_chat_id=update.message.chat_id, message_id=update.message.message_id)
 
-def photo_upd(bot, update):
-        bot.forward_message(chat_id = 367989051, from_chat_id=update.message.chat_id, message_id=update.message.message_id)
-        bot.send_message(chat_id=update.message.chat_id, text="Thank you for your mark. It will be added to the system soon.")
-    
-updater = Updater(token='922359953:AAEBvc5uRRO7qMPD6CI8TgdtHHz-oev42uQ')
+def music_send(bot, update):
+        rand_music = random.randint(1,Pyre().num_get())
+        music = Pyre().music_get(rand_music)
+
+        bot.send_audio(chat_id = update.message.chat_id, audio=music)
+
+updater = Updater(token='1053633256:AAEphnCCmpK9-J9cVgnLcExOol1_w5wzEZY')
 dispatcher = updater.dispatcher
 
-echo2_handler = MessageHandler(Filters.photo, photo_upd)
+echo2_handler = MessageHandler(Filters.audio, music_upd)
 dispatcher.add_handler(echo2_handler)
 
 echo1_handler = MessageHandler(Filters.text, text_upd)
 dispatcher.add_handler(echo1_handler)
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+music_handler = CommandHandler('music', music_send)
+dispatcher.add_handler(music_handler)
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
